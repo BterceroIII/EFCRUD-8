be15e6a7915d4bdfd76b8070e40e8b30337f6422
BterceroIII <b1tercero.cruz@gmail.com>
2024-05-02 23:01:32 -0600

    Configuracion de DBContext

---
+++ Data/AppDBContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EFCRUD8.Models;
+
+namespace EFCRUD8.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<Empleado> Empleados { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Empleado>(tb =>
+            {
+                tb.HasKey(col => col.IdEmpleado);
+
+                tb.Property(col => col.IdEmpleado)
+                .UseIdentityColumn()
+                .ValueGeneratedOnAdd();
+
+                tb.Property(col => col.Nombre).HasMaxLength(20);
+                tb.Property(col => col.Apellido).HasMaxLength(20);
+                tb.Property(col => col.Correo).HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<Empleado>().ToTable("Empleado");
+        }
+    }
+}
