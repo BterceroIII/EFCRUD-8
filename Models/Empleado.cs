fd3c6949085f7865f7de3a7fd5bf6d62d085f70d
BterceroIII <b1tercero.cruz@gmail.com>
2024-05-02 22:42:05 -0600

    EmpleadoModel

---
+++ Models/Empleado.cs
@@ -0,0 +1,18 @@
+﻿namespace EFCRUD8.Models
+{
+    public class Empleado
+    {
+        public int IdEmpleado { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        public string Correo { get; set; }
+
+        public DateOnly FechaContrato { get; set; }
+
+        public bool Activo { get; set; }
+
+    }
+}
